1379cab1f316fdf2f4f73f5a4b0bd515dc094212
Bruno Zambuze <zambuzesilva@hotmail.com>
2024-09-05 09:53:40 -0300

    Criação e integração com o banco de dados

--- ToDoList/src/Main/Principal.java
+++ ToDoList/src/Main/Principal.java
@@ -1,18 +1,10 @@
 package Main;
-import DAO.FabricaConexao;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		try{
-			Connection conexao = FabricaConexao.getConexao();
-			FabricaConexao.criarTabelas();
-		} catch(SQLException e) {
-			System.out.println(e.getMessage());
-		}
+		
 		
 	}
 	
